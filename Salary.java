7c7f0cfcd5b46cdccffe01f918b9ac10954dcf7a
NoChaser98 <122207022+NoChaser98@users.noreply.github.com>
2023-04-30 00:09:14 -0400

    Salary

---
+++ Salary.java
@@ -0,0 +1,18 @@
+// import statement to be able to 
+// in the program
+import java.util.Scanner;
+
+public class Salary {
+	public static void main(Strings[] args) {
+		//Create a Scanner object called scnr to read input
+		//from the keyboard i.e. standard input (System.in)
+		Scanner scnr = new Scanner(System.in);
+		System.out.print("Enter the wage $");
+		//delcaring the int variable wage
+		//initialize wage to the value that scnr
+		//will give us from the user
+		int wage = scnr.nextInt();
+
+	}
+	
+}
